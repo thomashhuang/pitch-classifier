87676e7bdde10be9f4a181f29e931fa0d52869fb
Thomas Huang <thomas.huang2017@gmail.com>
2019-10-18 01:20:39 -0500

    Finish data retrieval script

--- util/game_retrieval.py
+++ util/game_retrieval.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 pitcher_id = 543294
 
-
-r = requests.get("http://www.brooksbaseball.net/tabs.php?player={}&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=gl&s_type=2&startDate=03/30/2007&endDate=10/17/2019&balls=-1&strikes=-1&b_hand=-1".format(pitcher_id))
+r = requests.get("http://www.brooksbaseball.net/tabs.php?player={}&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=gl&s_type=2&startDate=03/30/2008&endDate=10/17/2019&balls=-1&strikes=-1&b_hand=-1".format(pitcher_id))
 
 
 fx_url_base = "http://www.brooksbaseball.net/pfxVB/tabdel_expanded.php?pitchSel={}&game={}/&s_type=3&h_size=700&v_size=500"
@@ -19,7 +18,25 @@ for line in r.text.splitlines():
         game_ids.append(line[game_id_begin:game_id_end])
 
 # r = requests.get(fx_url_base.format(pitcher_id, game_ids[0]))
+print("Found {} games.".format(len(game_ids)))
 
+game_dfs = []
+
+games_processed = 0
 for game in game_ids:
     df = pd.read_html(fx_url_base.format(pitcher_id, game))
+    game_dfs.append(df[0])
+    games_processed += 1
+    print("Processed game {}/{}".format(games_processed, len(game_ids)))
+
+full_pitch_history = pd.concat(game_dfs, ignore_index=True)
+full_pitch_history = full_pitch_history[full_pitch_history.des != "Intent Walk"]
+
+print("Loaded {} pitches".format(full_pitch_history.count()))
+
+full_pitch_history = full_pitch_history[["mlbam_pitch_name", "spin", "ftime", "pfx_x", "pfx_z", "start_speed"]]
+
+data_path = "../data/{}.csv".format(pitcher_id)
+
+full_pitch_history.to_csv(data_path)
 
