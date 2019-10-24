a7cf0cef1dc428a1aa3feecb072dbb063eade76c
Thomas Huang <thomas.huang2017@gmail.com>
2019-10-24 11:42:52 -0500

    Implement PitchSet

--- src/pitch_set.cpp
+++ src/pitch_set.cpp
@@ -1,5 +1,6 @@
 #include "pitch_set.h"
 
+#include <stdlib.h>
 #include <vector>
 
 #include "pitch.h"
@@ -7,14 +8,33 @@
 using namespace pitch_classifier;
 
 void PitchSet::add_pitch(const Pitch& p) {
-  
+  this->pitches.push_back(p);
+}
+
+void PitchSet::clear() {
+  this->pitches.clear();
 }
 
 size_t PitchSet::size() {
-  return 0;
+  return this->pitches.size();
+}
+
+Pitch& PitchSet::operator[](size_t n) {
+  return this->pitches[n];
 }
  
-PitchSet* PitchSet::generate_test_set(int size) {
-  return NULL;
+PitchSet* PitchSet::generate_test_set(size_t size) {
+  PitchSet* test_set = new PitchSet();
+
+  for (size_t i = 0; i < size; i++) {
+    size_t index_to_remove = (size_t) (rand() * this->pitches.size());
+
+    Pitch& p = this->pitches[index_to_remove];
+    test_set->add_pitch(p);
+
+    this->pitches.erase(this->pitches.begin() + index_to_remove);
+  }
+
+  return test_set;
 }
   
--- src/pitch_set.h
+++ src/pitch_set.h
@@ -11,14 +11,21 @@ namespace pitch_classifier {
    public:
     void add_pitch(const Pitch& p);
 
+    void clear();
+
     size_t size();
 
-    PitchSet* generate_test_set(int size);
+    Pitch& operator[](size_t n);
+
+    /**
+     * Instantiates a new PitchSet on the heap which must be deleted after use.
+     * Takes size random pitches from the set to create a test set.
+     */
+    PitchSet* generate_test_set(size_t size);
 
    private:
     std::vector<Pitch> pitches;
   };
-  
 }
 
 #endif
